f094777bd1b59ca040750efe7de54f9ffbe4af92
Puran Zhang <puranzhang@Purans-MacBook-Pro.local>
2015-10-21 12:17:05 -0400

    modify shortcut files

---
+++ shortcuts/run_com4.bat
@@ -0,0 +1,18 @@
+START /WAIT start_com4.bat_Shortcut.lnk
+START /WAIT "Game" "C:UDK\UDK-2013-09\Binaries\Win32\UDK" "C:\UDK\UDK-2013-09\UDKGame\Content\Maps\FSB_Spring_2015\mazeA_phase2_coins.udk?game=mymod.FeelSpaceGame"
+
+
+:LOOP
+tasklist /FI "IMAGENAME eq UDK.exe" 2>NUL | find ":">NUL
+IF "%ERRORLEVEL%"=="0" (
+  (GOTO CONTINUE)
+) ELSE (
+  ECHO UDK is still running
+  ping -n 1 127.0.0.1 > nul
+  (GOTO LOOP)
+)
+
+
+:CONTINUE
+taskkill /im start_arg.exe
+START stop_com4.bat_Shortcut.lnk
